c39ae93786598b9086d477c08b94dadd3a8e5c5c
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-07-10 17:20:02 +0900

    Add util usefull for sequential training

--- nnmnkwii/utils/__init__.py
+++ nnmnkwii/utils/__init__.py
@@ -21,3 +21,34 @@ def trim_zeros_frames(x):
     T_trimed = np.sum([s > 0])
     assert T_trimed <= T
     return x[:T_trimed]
+
+
+def adjast_frame_length(x, y, pad=True, ensure_even=False):
+    Tx, Dx = x.shape
+    Ty, Dy = y.shape
+    assert Dx == Dy
+
+    if pad:
+        T = max(Tx, Ty)
+        if ensure_even:
+            T = T + 1 if T % 2 != 0 else T
+    else:
+        T = min(Tx, Ty)
+        if ensure_even:
+            T = T - 1 if T % 2 != 0 else T
+
+    if Tx != T:
+        if Tx < T:
+            x = np.vstack(
+                (x, np.zeros((T - Tx, x.shape[-1]), dtype=x.dtype)))
+        else:
+            x = x[:T]
+
+    if Ty != T:
+        if Ty < T:
+            y = np.vstack(
+                (y, np.zeros((T - Ty, y.shape[-1]), dtype=y.dtype)))
+        else:
+            y = y[:T]
+
+    return x, y
