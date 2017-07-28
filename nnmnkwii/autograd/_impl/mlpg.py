Fix mlpg grad computation bug and add tests
caused by silly typo...

nnmnkwii/autograd/_impl/mlpg.py
@@ -60,18 +60,20 @@ class MLPG(Function):
 
             # R: \sum_{l} W_{l}^{T}P_{l}W_{l}
             R = bm.zeros(sdw, sdw, T)  # overwritten in the loop
+
+            # dtype = np.float64 for bandmat
+            precisions = np.zeros((len(self.windows), T), dtype=np.float64)
+
             for win_idx, win_mat in enumerate(win_mats):
-                # cast to float64 for bandmat
-                precisions = 1 / \
-                    variance_frames[:, win_idx * self.static_dim +
-                                    d].numpy().astype(np.float64)
+                precisions[win_idx] = 1 / \
+                    variance_frames[:, win_idx * self.static_dim + d].numpy()
 
                 bm.dot_mm_plus_equals(win_mat.T, win_mat,
-                                      target_bm=R, diag=precisions)
+                                      target_bm=R, diag=precisions[win_idx])
 
             for win_idx, win_mat in enumerate(win_mats):
                 # r: W_{l}^{T}P_{l}
-                r = bm.dot_mm(win_mat.T, bm.diag(precisions))
+                r = bm.dot_mm(win_mat.T, bm.diag(precisions[win_idx]))
 
                 # grad_{d, l} = R^{-1r}
                 grad = np.linalg.solve(R.full(), r.full())

tests/test_mlpg.py
@@ -7,18 +7,45 @@ import numpy as np
 
 
 def test_mlpg_gradcheck():
-    static_dim = 24
+    # MLPG is performed dimention by dimention, so static_dim 1 is enough, 2 just for in
+    # case.
+    static_dim = 2
     T = 10
-    windows = [
-        (0, 0, np.array([1.0])),
-        # TODO: maybe we need to be careful abount edges? (t=0, t=T-1)
-        # (1, 1, np.array([-0.5, 0.0, 0.5])),
+
+    windows_set = [
+        # Static
+        [
+            (0, 0, np.array([1.0])),
+        ],
+        # Static + delta
+        [
+            (0, 0, np.array([1.0])),
+            (1, 1, np.array([-0.5, 0.0, 0.5])),
+        ],
+        # Static + delta + deltadelta
+        [
+            (0, 0, np.array([1.0])),
+            (1, 1, np.array([-0.5, 0.0, 0.5])),
+            (1, 1, np.array([1.0, -2.0, 1.0])),
+        ],
     ]
-    means = Variable(torch.rand(T, static_dim * len(windows)),
-                     requires_grad=True)
-    variances = torch.rand(static_dim * len(windows)
-                           ).expand(T, static_dim * len(windows))
 
-    inputs = (means,)
+    for windows in windows_set:
+        torch.manual_seed(1234)
+        means = Variable(torch.rand(T, static_dim * len(windows)),
+                         requires_grad=True)
+        inputs = (means,)
 
-    assert gradcheck(MLPG(static_dim, variances, windows), inputs, eps=1e-4)
+        # Unit variances case
+        variances = torch.ones(static_dim * len(windows)
+                               ).expand(T, static_dim * len(windows))
+
+        assert gradcheck(MLPG(static_dim, variances, windows),
+                         inputs, eps=1e-3, atol=1e-3)
+
+        # Rand variances case
+        variances = torch.rand(static_dim * len(windows)
+                               ).expand(T, static_dim * len(windows))
+
+        assert gradcheck(MLPG(static_dim, variances, windows),
+                         inputs, eps=1e-3, atol=1e-3)