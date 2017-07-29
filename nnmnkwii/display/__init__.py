d4176c0ed603406b46b0476f89acaab8f2d1d4c2
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-07-29 17:58:08 +0900

    Display package should be addressed later

---
+++ nnmnkwii/display/__init__.py
@@ -0,0 +1,12 @@
+"""
+Visualization utilities
+=======================
+
+.. note:
+    comming soon
+
+.. autosummary::
+    :toctree: generated/
+"""
+
+from __future__ import with_statement, print_function, absolute_import
