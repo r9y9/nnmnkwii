1a871fb1d2934c982328014db2574db2ad612846
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-08-24 21:46:41 +0900

    Tweak

--- nnmnkwii/preprocessing/__init__.py
+++ nnmnkwii/preprocessing/__init__.py
@@ -6,10 +6,7 @@ from .generic import adjast_frame_length, scale, minmax_scale
 from .generic import meanvar, meanstd, minmax
 from .f0 import interp1d
 from .modspec import modspec, modphase
-from .alignment import DTWAligner, IterativeDTWAligner
-
 
 __all__ = ['delta_features', 'trim_zeros_frames', 'remove_zeros_frames',
            'adjast_frame_length', 'scale', 'minmax_scale', 'meanvar',
-           'meanstd', 'minmax', 'interp1d', 'modspec', 'modphase',
-           'DTWAligner', 'IterativeDTWAligner']
+           'meanstd', 'minmax', 'interp1d', 'modspec', 'modphase']
