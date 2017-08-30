60947fd9ef0e76cbb5c16e47e2b8f4431d18ca67
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-08-30 14:16:21 +0900

    Fix #26

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,6 +1,12 @@
 Change log
 ==========
 
+dev
+---
+
+- `#26`_: Add tests for GMM based voice conversion if swap=True
+- `#25`_: fix typo in nnmnkwii/baseline/gmm.py
+
 v0.0.3 <2017-08-26>
 -------------------
 
@@ -26,3 +32,5 @@ v0.0.1 <2017-08-14>
 .. _#21: https://github.com/r9y9/nnmnkwii/pull/21
 .. _#22: https://github.com/r9y9/nnmnkwii/issues/22
 .. _#23: https://github.com/r9y9/nnmnkwii/pull/23
+.. _#25: https://github.com/r9y9/nnmnkwii/pull/25
+.. _#26: https://github.com/r9y9/nnmnkwii/issues/26
--- tests/test_baseline.py
+++ tests/test_baseline.py
@@ -4,16 +4,12 @@ from nnmnkwii.baseline.gmm import MLPG
 import numpy as np
 from sklearn.mixture import GaussianMixture
 from os.path import join, dirname
+from numpy.linalg import norm
 
 DATA_DIR = join(dirname(__file__), "data")
 
 
-def test_diffvc():
-    # MLPG is performed dimention by dimention, so static_dim 1 is enough, 2 just for in
-    # case.
-    static_dim = 2
-    T = 10
-
+def _get_windows_set():
     windows_set = [
         # Static
         [
@@ -31,8 +27,17 @@ def test_diffvc():
             (1, 1, np.array([1.0, -2.0, 1.0])),
         ],
     ]
+    return windows_set
 
-    for windows in windows_set:
+
+def test_diffvc():
+    # MLPG is performed dimention by dimention, so static_dim 1 is enough, 2 just for in
+    # case.
+    static_dim = 2
+    T = 10
+
+    for windows in _get_windows_set():
+        np.random.seed(1234)
         src_mc = np.random.rand(T, static_dim * len(windows))
         tgt_mc = np.random.rand(T, static_dim * len(windows))
 
@@ -49,3 +54,34 @@ def test_diffvc():
 
         assert mc_converted1.shape == (T, static_dim)
         assert mc_converted2.shape == (T, static_dim)
+
+        src_mc = src_mc[:, :static_dim]
+        tgt_mc = tgt_mc[:, :static_dim]
+        assert norm(tgt_mc - mc_converted1) < norm(src_mc - mc_converted1)
+
+
+def test_gmmmap_swap():
+    static_dim = 2
+    T = 10
+    windows = _get_windows_set()[-1]
+
+    np.random.seed(1234)
+    src_mc = np.random.rand(T, static_dim * len(windows))
+    tgt_mc = np.random.rand(T, static_dim * len(windows))
+
+    # pseudo parallel data
+    XY = np.concatenate((src_mc, tgt_mc), axis=-1)
+    gmm = GaussianMixture(n_components=4)
+    gmm.fit(XY)
+
+    paramgen = MLPG(gmm, windows=windows, swap=False)
+    swap_paramgen = MLPG(gmm, windows=windows, swap=True)
+
+    mc_converted1 = paramgen.transform(src_mc)
+    mc_converted2 = swap_paramgen.transform(tgt_mc)
+
+    src_mc = src_mc[:, :static_dim]
+    tgt_mc = tgt_mc[:, :static_dim]
+
+    assert norm(tgt_mc - mc_converted1) < norm(src_mc - mc_converted1)
+    assert norm(tgt_mc - mc_converted2) > norm(src_mc - mc_converted2)
