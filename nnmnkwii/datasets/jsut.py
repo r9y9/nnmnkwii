e8dc08b4426b96ae916528dd6944c38abc7a9a1b
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-11-02 22:53:34 +0900

    Add bulitin data sources for JSUT dataset
    fixes #50

---
+++ nnmnkwii/datasets/jsut.py
@@ -0,0 +1,140 @@
+from __future__ import with_statement, print_function, absolute_import
+
+from nnmnkwii.datasets import FileDataSource
+
+import numpy as np
+from os.path import join, splitext, isdir, exists
+from os import listdir
+from warnings import warn
+
+available_subsets = ["basic5000",
+                     "countersuffix26",
+                     "loanword128",
+                     "onomatopee300",
+                     "precedent130",
+                     "repeat500",
+                     "travel1000",
+                     "utparaphrase512",
+                     "voiceactress100",
+                     ]
+
+
+class BaseDataSource(FileDataSource):
+    def __init__(self, data_root, subset="basic5000"):
+        self.subset = subset
+        self.data_root = data_root
+        transcription_path = join(data_root, subset, "transcript_utf8.txt")
+        if not exists(transcription_path):
+            raise RuntimeError(
+                "transcript_utf8.txt doesn't exists at \"{}\"".format(transcription_path))
+
+        with open(transcription_path, "rb") as f:
+            names, transcriptions = [], []
+            for line in f:
+                line = line.decode("utf-8")
+                if ":" not in line:
+                    continue
+                name, transcription = line.strip().split(":")
+
+                # Hack
+                if self.subset == "basic5000" and "BASIC4992" in name:
+                    name = name.replace("BASIC4992", "BASIC5000")
+                elif self.subset == "voiceactress100":
+                    name = name[:12] + "100_" + name[12:]
+
+                names.append(name)
+                transcriptions.append(transcription)
+        self.names = np.asarray(names)
+        self.transcriptions = np.asarray(transcriptions)
+
+    def validate(self):
+        wav_dir = join(self.data_root, self.subset, "wav")
+        if not isdir(wav_dir):
+            raise RuntimeError("{} doesn't exist.".format(wav_dir))
+        miss_indices = []
+        for idx, name in enumerate(self.names):
+            wav_path = join(wav_dir, name + ".wav")
+            if not exists(wav_path):
+                miss_indices.append(idx)
+
+        if len(miss_indices) > 0:
+            warn("{}/{} wav files were missing in subset {}.".format(
+                len(miss_indices), len(self.names), self.subset))
+
+        self.names = np.delete(self.names, miss_indices)
+        self.transcriptions = np.delete(self.transcriptions, miss_indices)
+
+    def collect_files(self, is_wav):
+        if is_wav:
+            wav_dir = join(self.data_root, self.subset, "wav")
+            wav_paths = list(map(lambda name: join(wav_dir, name + ".wav"), self.names))
+            return np.asarray(wav_paths)
+        else:
+            return self.transcriptions
+
+    def __len__(self):
+        return len(self.names)
+
+
+class _JSUTFileDataSource(FileDataSource):
+    def __init__(self, data_root, subsets, is_wav, validate):
+        for subset in subsets:
+            if subset not in available_subsets:
+                raise ValueError(
+                    "Unknown subset '{}'. It should be one of {}".format(
+                        subset, available_subsets))
+
+        self.data_root = data_root
+        self.subsets = subsets
+        self.sub_data_sources = []
+        for subset in subsets:
+            d = BaseDataSource(data_root, subset)
+            if validate:
+                d.validate()
+            self.sub_data_sources.append(d)
+        self.is_wav = is_wav
+
+    def collect_files(self):
+        paths = []
+        for sub_data_source in self.sub_data_sources:
+            paths.extend(sub_data_source.collect_files(self.is_wav))
+        return np.asarray(paths)
+
+
+class TranscriptionDataSource(_JSUTFileDataSource):
+    """Transcription data source for JSUT dataset.
+
+    The data source collects text transcriptions from JSUT.
+    Users are expected to inherit the class and implement ``collect_features``
+    method, which defines how features are computed given a transcription.
+
+    Args:
+        data_root (str): Data root.
+        subsets (list): Subsets.  Supported names of subset are ``basic5000``,
+          ``countersuffix26``, ``loanword128``, ``onomatopee300``,
+          ``precedent130``, ``repeat500``, ``travel1000``, ``utparaphrase512``.
+          and ``voiceactress100``. Default is ["basic5000"].
+    """
+
+    def __init__(self, data_root, subsets=["basic5000"], validate=True):
+        super(TranscriptionDataSource, self).__init__(
+            data_root, subsets, False, validate)
+
+
+class WavFileDataSource(_JSUTFileDataSource):
+    """Wav file data source for JSUT dataset.
+
+    The data source collects wav files from JSUT.
+    Users are expected to inherit the class and implement ``collect_features``
+    method, which defines how features are computed given a wav file path.
+
+    Args:
+        data_root (str): Data root.
+        subsets (list): Subsets.  Supported names of subset are ``basic5000``,
+          ``countersuffix26``, ``loanword128``, ``onomatopee300``,
+          ``precedent130``, ``repeat500``, ``travel1000``, ``utparaphrase512``.
+          and ``voiceactress100``. Default is ["basic5000"].
+    """
+
+    def __init__(self, data_root, subsets=["basic5000"], validate=True):
+        super(WavFileDataSource, self).__init__(data_root, subsets, True, validate)
--- tests/test_real_datasets.py
+++ tests/test_real_datasets.py
@@ -14,7 +14,7 @@ import pyworld
 from nnmnkwii.preprocessing import trim_zeros_frames
 
 # Data source implementations
-from nnmnkwii.datasets import cmu_arctic, voice_statistics, ljspeech, vcc2016
+from nnmnkwii.datasets import cmu_arctic, voice_statistics, ljspeech, vcc2016, jsut
 
 # Tests marked with "require_local_data" needs data to be downloaded.
 
@@ -81,6 +81,18 @@ def test_vcc2016_dummy():
 
     __test_invalid_speaker()
     __test_nodir(data_source)
+
+
+def test_jsut_dummy():
+    data_sources = [jsut.TranscriptionDataSource,
+                    ljspeech.WavFileDataSource]
+
+    for data_source in data_sources:
+        @raises(RuntimeError)
+        def f(source):
+            source("dummy")
+
+        f(data_source)
 
 
 @attr("require_local_data")
@@ -312,3 +324,60 @@ def test_vcc2016():
         DATA_DIR, speakers=["SF1", "SF2"], max_files=None)
     X = FileSourceDataset(data_source)
     assert len(X) == 162 * 2
+
+
+@attr("require_local_data")
+@attr("require_jsut")
+def test_ljspeech():
+    DATA_DIR = join(expanduser("~"), "data", "jsut_ver1")
+    if not exists(DATA_DIR):
+        warn("Data doesn't exist at {}".format(DATA_DIR))
+        return
+
+    class MyTextDataSource(jsut.TranscriptionDataSource):
+        def __init__(self, data_root, subsets):
+            super(MyTextDataSource, self).__init__(data_root, subsets)
+
+        def collect_features(self, text):
+            return text
+
+    data_source = MyTextDataSource(DATA_DIR, subsets=["basic5000"])
+    X1 = FileSourceDataset(data_source)
+    assert X1[0] == u"水をマレーシアから買わなくてはならないのです。"
+
+    data_source = MyTextDataSource(DATA_DIR, subsets=["travel1000"])
+    X2 = FileSourceDataset(data_source)
+    assert X2[0] == u"あなたの荷物は、ロサンゼルスに残っています。"
+
+    # Multiple subsets
+    data_source = MyTextDataSource(DATA_DIR, subsets=["basic5000", "travel1000"])
+    X3 = FileSourceDataset(data_source)
+    assert X3[0] == u"水をマレーシアから買わなくてはならないのです。"
+    assert len(X3) == len(X1) + len(X2)
+
+    # All subsets
+    data_source = MyTextDataSource(DATA_DIR, subsets=jsut.available_subsets)
+    X = FileSourceDataset(data_source)
+    # As of 2017/11/2. There were 30 missing wav files.
+    # This should be 7696
+    assert len(X) == 7666
+
+    class MyWavFileDataSource(jsut.WavFileDataSource):
+        def __init__(self, data_root, subsets):
+            super(MyWavFileDataSource, self).__init__(data_root, subsets)
+            self.alpha = pysptk.util.mcepalpha(48000)
+
+        def collect_features(self, path):
+            fs, x = wavfile.read(path)
+            assert fs == 48000
+            x = x.astype(np.float64)
+            f0, timeaxis = pyworld.dio(x, fs, frame_period=5)
+            f0 = pyworld.stonemask(x, f0, timeaxis, fs)
+            spectrogram = pyworld.cheaptrick(x, f0, timeaxis, fs)
+            spectrogram = trim_zeros_frames(spectrogram)
+            mc = pysptk.sp2mc(spectrogram, order=24, alpha=self.alpha)
+            return mc.astype(np.float32)
+
+    data_source = MyWavFileDataSource(DATA_DIR, subsets=["basic5000"])
+    X = FileSourceDataset(data_source)
+    print(X[0].shape)
