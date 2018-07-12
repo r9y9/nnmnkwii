5b88a9a392a56d1808133273162e494f4db5889e
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-07-12 21:29:44 +0900

    2018年  7月 12日 木曜日 21:29:44 JST

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.0.15+e9f07a3',
+    VERSION: '0.0.16+6861bbb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
