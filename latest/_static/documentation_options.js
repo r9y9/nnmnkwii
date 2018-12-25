b6ad97ad1b8245f2c0a2c68f677935588899cb3f
Ryuichi Yamamoto <zryuichi@gmail.com>
2018-12-26 00:08:33 +0900

    latest

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.17+81b22c6',
+    VERSION: '0.0.18+3162967',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
