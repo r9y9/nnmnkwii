8faf5d689fc697043960c707bc7444a26bbc80b3
Ryuichi Yamamoto <zryuichi@gmail.com>
2019-07-06 18:13:27 +0900

    2019年 7月 6日 土曜日 18時13分27秒 KST

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.18+3162967',
+    VERSION: '0.0.20+c09ba07',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
